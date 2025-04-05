d56f2d43c2d32fb4f3be2ecc4ec4e29ca2355fe5
PRIYAM <you@example.com>
2025-04-05 19:15:27 +0530

    debugged some issues

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5005',
-        changeOrigin: true,
-      }
-    }
-  }
 });
