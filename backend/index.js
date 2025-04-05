a59d0b9b63825b41819bfecbaecb486283794b75
PRIYAM <you@example.com>
2025-04-05 19:47:20 +0530

    added cors settings

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,13 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
+    methods: ["GET", "POST"],
+  })
+);
+
 app.get("/", async (req, res) => {
   res.send("Working");
 });
